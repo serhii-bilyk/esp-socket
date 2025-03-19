3819df3e09f9883d46eb2d0c67d79f93afe47ed0
Serhii Bilyk <serhii@theapply.ai>
2025-03-19 16:54:02 +0200

    serve web page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket
+import network
 
 def client_program():
     host = '192.168.0.102'  # as both code is running on same pc
@@ -19,4 +20,24 @@ def client_program():
 
     client_socket.close()  # close the connection
 
-client_program()
+# client_program()
+
+def wifi():
+    
+    # Create an access point (AP) instance
+    ap = network.WLAN(network.AP_IF)
+
+    # Activate the access point
+    ap.active(True)
+
+    # Set the SSID and password
+    ssid = 'ESP8266-AP'
+    password = '123456789'
+
+    # Configure the access point
+    ap.config(essid=ssid, password=password)
+
+    # Print the IP address of the access point
+    print('Access Point started with IP:', ap.ifconfig()[0])
+    
+wifi()
